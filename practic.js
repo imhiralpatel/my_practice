0c1dd3c4b5bdc1f6f1fd2f0f77d41c48bb6c9ee7
Hiral Patel <hiralpatel2918@gmail.com>
2026-04-06 19:34:17 +0530

    first commit

---
+++ practic.js
@@ -0,0 +1,29 @@
+alert("Welcome to JS");
+
+let a = prompt("Enter a value :");
+
+const b = 2;
+
+var c = BigInt(143);
+
+console.log("value of a : ", a);
+
+console.log("value of b : ", b);
+
+console.log("value of c : ", c);
+
+console.log("DataType of bigint: ", typeof c);
+
+var g;
+let h = null;
+console.log("Update value of g : ", g);
+console.log("DataType of var h : ", typeof h);
+
+let titleName = "Welcome to in js Programming...";
+console.log("DataType of ", titleName, "  : ", typeof titleName);
+
+let totalAmount = 786.17;
+console.log("DataType of ", totalAmount," : ", typeof totalAmount);
+ 
+let isFollow = true;
+console.log("DataType of isFollow : ", typeof isFollow);
---
+++ practic_two.js
@@ -0,0 +1,103 @@
+let a = prompt("Enter a value of a:");
+let b = 5;
+
+// Operator
+
+console.log("value of a : ", a);
+
+console.log("value of b : ", b);
+
+
+let sum = a + b;
+
+let sub = a - b;
+
+let mul = a * b;
+
+let div = a/b;
+
+let Square = a**b;
+
+let modul = a % b;
+
+
+console.log("value of sum : ", sum);
+
+console.log("value of substitute : ", sub);
+
+console.log("value of multiply : ", mul);
+
+console.log("value of division : ", div);
+
+console.log("value of square : ", Square);
+
+console.log("value of modul : ", modul);
+
+a =5;
+
+console.log("value of a + b : ", a+=b);
+
+console.log("value of a++ : ", a++);
+
+console.log("value of ++a : ", ++a);
+
+console.log("value of b-- : ", b--);
+
+console.log("value of --b : ", --b);
+
+console.log("value of a *= b : ", a *= b);
+
+console.log("value of a >= b : ", a >= b);
+
+console.log("value of a > b : ", a > b);
+
+console.log("value of a <=  b : ", a <= b);
+
+console.log("value of a < b : ", a < b);
+
+a =5;
+b= 6;
+
+console.log("value of a == b : ", a == b);
+
+console.log("value of a != b : ", a != b);
+
+console.log("value of a === b : ", a === b);
+
+console.log("value of a !== b : ", a !== b);
+
+a = "5";
+b = "5";
+console.log("value of a === b : ", a === b);
+
+
+console.log("value of a -= b : ", b-=a);
+
+
+// Condition Statement
+
+const age = prompt("Enter your Age :");
+
+console.log("Age is :", age);
+
+if(age < 18) // If Statement
+{
+    console.log("Junior");
+}
+else if(age > 60) // else-if statement
+{
+    console.log("Senior");
+}
+else // if-else statement
+{
+    console.log("Middle");
+}
+
+// Ternary Operator
+const result = age >= 18 ? "Adult" : "Not Adult";
+
+console.log(result);
+
+
+
+
